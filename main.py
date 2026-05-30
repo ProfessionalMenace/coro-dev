18ce059d5c7b92d0dd1b82009f4ded0d8b9d08b5
Foo45 <rowan.stjames.cooper@gmail.com>
2026-05-30 11:14:31 -0400

    create sql database and a few commands

--- main.py
+++ main.py
@@ -17,27 +17,126 @@ config.close()
 
 TOKEN = json.loads(open("./config/secrets.json", "r").read())["BOT TOKEN"]
 BOT_COLOR = config_data["BOT COLOR"]
-SERVER_ID = config_data["SERVER ID"]
+SERVER_ID = int(config_data["SERVER ID"])
+
+MOD_ROLE_ID = int(config_data["MOD ROLE ID"])
 
 confessions = Database("config/confessions.db")
 
 guild = discord.Object(id=SERVER_ID)
 class MyClient(discord.Client):
-    def __init__(self, *, intents: discord.Intents):
-        super().__init__(intents=intents)
-        self.CoroboCult: typing.Optional[discord.Guild] = None # CoroboCult Server
-        self.tree = app_commands.CommandTree(self, allowed_contexts=app_commands.AppCommandContext(guild=True,dm_channel=False,private_channel=False))
-    async def setup_hook(self):
-        print([command.name for command in await self.tree.sync(guild=guild)])
-        print([command.name for command in await self.tree.sync()])
+	def __init__(self, *, intents: discord.Intents):
+		super().__init__(intents=intents)
+		self.CoroboCult: typing.Optional[discord.Guild] = None # CoroboCult Server
+		self.tree = app_commands.CommandTree(self, allowed_contexts=app_commands.AppCommandContext(guild=True,dm_channel=False,private_channel=False))
+	async def setup_hook(self):
+		print([command.name for command in await self.tree.sync(guild=guild)])
+		print([command.name for command in await self.tree.sync()])
 
 intents = Intents.all()
 bot = MyClient(intents=intents)
 
+@tasks.loop(minutes=5)
+async def check_to_do():
+	confessions.save()
+
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} - {bot.user.id}') # pyright: ignore[reportOptionalMemberAccess]
-    for guild in bot.guilds:
-        if guild.id == SERVER_ID:
-            bot.CoroboCult = guild
-            
+	print(f'Logged in as {bot.user.name} - {bot.user.id}') # pyright: ignore[reportOptionalMemberAccess]
+	for guild in bot.guilds:
+		if guild.id == SERVER_ID:
+			bot.CoroboCult = guild
+
+@bot.tree.command(name="help", description="Get a list of commands or info on a single command")
+@app_commands.rename(cmd="command")
+@app_commands.describe(cmd="Get info on a specific command")
+@app_commands.guilds(SERVER_ID)
+async def _help(interaction: discord.Interaction, cmd: typing.Optional[str] = None):
+	output = ""
+	cmd_list = [command for command in bot.tree.walk_commands(guild=interaction.guild)]
+	if cmd is None:
+		output = "List of Commands\n```"
+		for command in cmd_list:
+			output += "/" + command.qualified_name + " - " + command.description + "\n"
+		output += "\n```"
+	else:
+		cmd_names = [command.qualified_name for command in cmd_list]
+		if cmd not in cmd_names:
+			output = "Please enter a valid cmd (use /help to find them)"
+		else:
+			command = [command for command in cmd_list if command.qualified_name == cmd][0]
+			output = "`/" + command.qualified_name + "`\n> " + command.description + "\n"
+			if isinstance(command, app_commands.commands.Group):
+				output += "Sub-commands:\n```\n"
+				for subcommand in command.commands:
+					output += "\n- " + subcommand.name + " > " + subcommand.description
+			else:
+				output += "Parameters:\n```\n"
+				for parameter in command.parameters:
+					output += "\n- " + ("<" if parameter.required else "[") + parameter.display_name + (">" if parameter.required else "]") + " > " + parameter.description
+			output += "\n```"
+	embed = discord.Embed(
+		title="Help: " + ("all" if cmd is None else cmd),
+		description=output,
+		color=int(BOT_COLOR, 16)
+	).set_footer(text = f"Made by CoroboCult Mod Team", icon_url = bot.user.avatar.url) # pyright: ignore[reportOptionalMemberAccess]
+	await interaction.response.send_message(embed=embed)
+@_help.autocomplete('cmd')
+async def command_autocomplete(interaction: discord.Interaction, current: str) -> typing.List[app_commands.Choice[str]]:
+	return [
+		app_commands.Choice(name=command.qualified_name, value=command.qualified_name)
+		for command in bot.tree.walk_commands(guild=interaction.guild) if current.upper() in command.qualified_name.upper()
+	][:25] if len(current) > 0 else [
+		app_commands.Choice(name=command.qualified_name, value=command.qualified_name)
+		for command in bot.tree.walk_commands(guild=interaction.guild)
+	][:25]
+
+confessions_group = app_commands.Group(name="confessions", description="Anonymous Confession Commands", guild_ids=[SERVER_ID])
+bot.tree.add_command(confessions_group)
+
+confessions_database_group = app_commands.Group(name="database", description="Access and Mofidy Confession Database", guild_ids=[SERVER_ID])
+confessions_group.add_command(confessions_database_group)
+
+@confessions_database_group.command(name = "create-macro", description="Create a new macro")
+@app_commands.checks.has_role(MOD_ROLE_ID)
+@app_commands.describe(
+	name = "Name of the macro, max 20 characters",
+	code = "Code of the macro"
+)
+async def confessions_database__add_macro (interaction: discord.Interaction, name: str, code: str):
+	await interaction.response.defer(ephemeral=True)
+	if len(name) >= 20:
+		return await interaction.followup.send("Name must be at most 20 characters")
+	if confessions.query(size = 1, query = "SELECT name FROM macros WHERE name = ?;", parameters = (name,)):
+		return await interaction.followup.send("Name is already in use")
+	confessions.create_macro(name = name, author_id = interaction.user.id, code = code)
+	await interaction.followup.send("Macro successfully created!")
+
+@confessions_database_group.command(name = "view-macro", description="View all macros, or get data on a specific one")
+@app_commands.checks.has_role(MOD_ROLE_ID)
+@app_commands.describe(name = "Get info on a specific macro")
+async def confessions_database__view_macros (interaction: discord.Interaction, name: typing.Optional[str] = None):
+	await interaction.response.defer(ephemeral=True)
+	if name is None:
+		output = []
+		for row in confessions.query(query = 'SELECT name, author_id FROM macros;'): # pyright: ignore[reportOptionalIterable]
+			output.append(f"`{row["name"]:>20}` - <@{row["author_id"]}>")
+		embed = discord.Embed(
+			title="Macros: " + ("all" if name is None else name),
+			description="\n".join(output),
+			color=int(BOT_COLOR, 16)
+		).set_footer(text = f"Made by CoroboCult Mod Team", icon_url = bot.user.avatar.url) # pyright: ignore[reportOptionalMemberAccess]
+		await interaction.followup.send(embed = embed)
+	else:
+		pass
+@confessions_database__view_macros.autocomplete('name')
+async def command_autocomplete_view_macros(interaction: discord.Interaction, current: str) -> typing.List[app_commands.Choice[str]]:
+	return [app_commands.Choice(name = value["name"], value = value["name"])
+				 for value in confessions.query(parameters = (f"%{current}%",), size = 25, query = '''
+																		SELECT name
+																		FROM macros
+																		WHERE name LIKE ?;
+				 ''')]
+
+
+bot.run(TOKEN)
--- sql.py
+++ sql.py
@@ -6,6 +6,7 @@ Utility file for manipulating and accessing the database
 
 import typing
 import sqlite3
+from collections.abc import Iterable
 
 class Database:
 
@@ -13,14 +14,89 @@ class Database:
 		self.path = path
 		self.connection = sqlite3.connect(self.path)
 		self.cursor = self.connection.cursor()
-		self.cursor.execute('''CREATE TABLE IF NOT EXISTS macros(
-											id INT PRIMARY_KEY,
-											name VARCHAR(20),
-											author_id INT,
-											code LONGTEXT
-		)''')
-		self.cursor.execute('''CREATE TABLE IF NOT EXISTS confessions(
+		self.cursor.execute('''CREATE TABLE IF NOT EXISTS macros (
+											name TEXT PRIMARY_KEY,
+											author_id INTEGER,
+											code TEXT,
+											query BOOLEAN
+		);''')
+		self.cursor.execute('''CREATE TABLE IF NOT EXISTS confessions (
 											id INT PRIMARY_KEY
-											author_id INT,
-											message_id INT
-		)''')
+											author_id INTEGER,
+											message_id INTEGER
+		);''')
+	
+	def save (self):
+		self.connection.commit()
+		self.connection.close()
+		self.connection = sqlite3.connect(self.path)
+		self.cursor = self.connection.cursor()
+	
+	def execute (self, *parameters: typing.Any, query: str):
+		if len(parameters) > 1: self.cursor.executemany(query, parameters)
+		else: self.cursor.execute(query, parameters)
+
+	@typing.overload
+	def query (self, /, query: str, parameters: typing.Any = None, size: typing.Literal[1] = 1) -> typing.Optional[sqlite3.Row]: ... # pyright: ignore[reportOverlappingOverload]
+
+	@typing.overload
+	def query (self, /, query: str, parameters: typing.Any = None, size: int = -1) -> typing.Generator[sqlite3.Row]: ...
+
+	def query (self, /, query: str, parameters: typing.Any = None, size: int = -1) -> typing.Union[typing.Generator[sqlite3.Row], typing.Optional[sqlite3.Row]]:
+		'''
+		Creates and returns the results of a query
+
+		If size is -1 or > 1, return all the results as a generator object yielding Row objects
+
+		If size is 1, return the result as a Row object
+		'''
+		if parameters: self.execute(parameters, query=query)
+		else: self.execute(query=query)
+		if size == 1: return self.cursor.fetchone()
+		i = 0
+		while size == -1 or i < size:
+			ret = self.cursor.fetchone()
+			if ret is None:
+				break
+			yield ret
+
+	@typing.overload
+	def use_macro (self, /, name: str, size: typing.Literal[1], parameters: typing.Any = ()) -> typing.Optional[sqlite3.Row]: ... # pyright: ignore[reportOverlappingOverload]
+
+	@typing.overload
+	def use_macro (self, /, name: str, size: int, parameters: typing.Any = ()) -> typing.Generator[sqlite3.Row]: ...
+
+	def use_macro (self, /, name: str, size: int = -1, parameters: typing.Any = ()) -> typing.Optional[typing.Union[typing.Generator[sqlite3.Row], typing.Optional[sqlite3.Row]]]:
+		macro = self.query(size = 1, parameters = (name,), query = '''
+										 SELECT code, query
+										 FROM macros
+										 WHERE name == ?;
+		''')
+		if macro:
+			if macro["query"]:
+				return self.query(query = macro["code"], parameters=parameters, size=size)
+			self.execute(parameters, query=macro["code"])
+	
+	def create_macro (self, /, name: str, author_id: int, code: str):
+		code = code.strip()
+		self.execute((name, author_id, code, code.index("SELECT") == 0), query='''
+										 INSERT INTO macros(name, author_id, code, query)
+							 			 VALUES (?, ?, ?, ?);
+		''')
+
+
+if __name__ == "__main__":
+	print("direct access enabled")
+	confessions = Database("config/confessions.db")
+	while True:
+		cmd = input("> ")
+		if cmd == "QUIT":
+			break
+		if cmd.find("SELECT") == 0:
+			size = int(input("Size: "))
+			ret = confessions.query(query=cmd, size = size)
+			if isinstance(ret, sqlite3.Row):
+				print(ret)
+			else:
+				for row in ret:
+					print(row)
